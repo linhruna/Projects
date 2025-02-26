bda75e4e83a2769ea79037757500a20bce048bf4
linhruna <nguyenlinh19042000@gmail.com>
2025-02-26 21:21:23 +0700

    Delete Warehouse_management directory

--- Warehouse_management/main/java/com/project/ims/model/dto/PartnerDTO.java
+++
@@ -1,9 +0,0 @@
-package com.project.ims.model.dto;
-
-import lombok.Data;
-
-@Data
-public class PartnerDTO {
-    private int partnerID;
-    private String name;
-}
--- Warehouse_management/main/java/com/project/ims/model/dto/SupplierDTOForAddProduct.java
+++
@@ -1,9 +0,0 @@
-package com.project.ims.model.dto;
-import lombok.Data;
-@Data
-public class SupplierDTOForAddProduct {
-			
-	    private int supplierId;
-	    private int productId;
-	}
-
